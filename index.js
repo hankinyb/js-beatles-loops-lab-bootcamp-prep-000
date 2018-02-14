7ce4643e538db3ee432844ed9b3d029e13947936
Yan Hankin <hankinyb@gmail.com>
2018-02-14 04:55:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,13 +17,3 @@ function johnLennonFacts() {
     return newFacts;
 }
 
-function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var shout = [];
-  var i = 0;
-  while (i < facts.length) {
-        shout.push(facts[i] +"!!!");
-        i++;
-  }    
-  return shout;
-}
