8e4153994fab35714044315cb4451659174d6515
Yan Hankin <hankinyb@gmail.com>
2018-02-14 05:18:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,18 +6,15 @@ function theBeatlesPlay(musicians, instruments) {
   return players;
 }
 
-*const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-
-function johnLennonFacts(facts) {
-  while () {
-    facts[n] + "!!!";
-  }
-  return facts;  
+function johnLennonFacts() {
+    var facts = [];
+    var newFacts = [];
+    var i = 0;
+    while (i < facts.length) {
+        facts.push(facts[i] +"!!!");
+        i++;
+    }
+    return facts;
 }
 
 function iLoveTheBeatles(i) {
