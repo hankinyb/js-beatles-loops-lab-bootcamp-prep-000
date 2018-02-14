552ce8a0330643f7397cfb974d331417351c180d
Yan Hankin <hankinyb@gmail.com>
2018-02-14 04:25:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(musicians, instruments) {
+  var players = [];
+  for (var i = 0; i < musicians.length; i++) {
+    players.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return players;
+}
+
+function johnLennonFacts(facts) {
+  
+  while (i < 4) {
+    i = 0;
+    i++;
+    return facts[i] + "!!!";
+  }    
+  
+}
